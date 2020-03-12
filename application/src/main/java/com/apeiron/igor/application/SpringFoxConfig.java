c46bc9b7e30c76fcd4a8df405e5fbbd5a08eb9e6
Mariya Pronina <mpronina@mera.ru>
2020-03-12 18:30:54 +0300

    WAVE-3: added swagger config

--- application/src/main/java/com/apeiron/igor/application/SpringFoxConfig.java
+++ application/src/main/java/com/apeiron/igor/application/SpringFoxConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)

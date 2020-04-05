0c01b8b60af152067e44c83c8d81ca9155469659
Владимир Михайлов <v.mikhailov23@gmail.com>
2020-04-06 02:31:11 +0500

    Валидация данных при регистрации
    Добавлена проверка введённых данных на удовлетвоение
    соответствующему регулярному выражению:
    
    Имя и фамилия - только латиница и/или кириллица (не менее 2 символов).
    Логин - латиница и/или цифры (8-15 символов).
    Пароль - латиница и/или цифры (6 и более символов).

--- web-client/src/main/js/igor-client/src/app/component/registration/registration.component.ts
+++ web-client/src/main/js/igor-client/src/app/component/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../service/authentication.service";
 import {NotificationsService} from "angular2-notifications";
 import {RestService} from "../../service/rest.service";
@@ -13,29 +13,54 @@ import {Router} from "@angular/router";
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  public name:string = "";
-  public lastName:string = "";
+  public name: string = "";
+  public lastName: string = "";
   public login: string = "";
   public password: string = "";
 
-  constructor(private _authenticationService:AuthenticationService,
-              private _notificationsService:NotificationsService,
-              private _restService:RestService,
+  constructor(private _authenticationService: AuthenticationService,
+              private _notificationsService: NotificationsService,
+              private _restService: RestService,
               private _router: Router) {
   }
 
   ngOnInit(): void {
   }
 
-  register():void {
-    let form:UserForm = new UserForm(this.name, this.lastName, this.login, this.password);
-    this._restService.registerUser(form)
-      .subscribe(data => {
-        this._notificationsService.success("", "Успешная регистрация");
-        this._router.navigate([Url.home]);
-      }, error => {
-        console.error(error);
-        this._notificationsService.error("", "Неверный логин или пароль")
-      })
+  register(): void {
+    let form: UserForm = new UserForm(this.name, this.lastName, this.login, this.password);
+
+    var valid = true;
+
+    if (!this.name.match("^[A-Za-zА-Яа-я]{2,}$")) {
+      this._notificationsService.error("", "Имя должно содержать не менее 2 символов (латиница и кириллица).");
+      valid = false;
+    }
+
+    if (!this.lastName.match("^[A-Za-zА-Яа-я]{2,}$")) {
+      this._notificationsService.error("", "Фамилия должна содержать не менее 2 символов (латиница и кириллица).");
+      valid = false;
+    }
+
+    if (!this.login.match("^[A-Za-z0-9]{8,15}$")) {
+      this._notificationsService.error("", "Логин должен содержать 8-15 символов (латинские буквы и цифры).");
+      valid = false;
+    }
+
+    if (!this.password.match("^[A-Za-z0-9]{6,}$")) {
+      this._notificationsService.error("", "Пароль должен содержать не менее 6 символов (латинские буквы и цифры).");
+      valid = false;
+    }
+
+    if (valid) {
+      this._restService.registerUser(form)
+        .subscribe(data => {
+          this._notificationsService.success("", "Успешная регистрация");
+          this._router.navigate([Url.home]);
+        }, error => {
+          console.error(error);
+          this._notificationsService.error("", "Данный логин уже существует");
+        })
+    }
   }
 }
